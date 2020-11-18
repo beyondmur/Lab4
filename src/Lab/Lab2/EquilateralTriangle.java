bc072629ce073277483ffef9e0d7b8d5781fe202
beyondmur <beyondmur@ukr.net>
2020-11-18 13:14:48 +0200

    Lab3

--- src/Lab/Lab2/EquilateralTriangle.java
+++ src/Lab/Lab2/EquilateralTriangle.java
@@ -1,19 +1,18 @@
 package Lab.Lab2;
-
 public class EquilateralTriangle extends Triangle {
 
     public EquilateralTriangle(double x1, double y1, double x2, double y2, double x3, double y3){
         super(x1, y1, x2, y2, x3, y3);
 
         if(!check_equilateralism())
-            throw new RuntimeException("The triangle is not equilateral triangle !");
+            throw new RuntimeException("Not an equilateral triangle found in the list!");
     }
 
     public EquilateralTriangle(Point pointOne, Point pointTwo, Point pointThree) {
         super(pointOne, pointTwo, pointThree);
 
         if(!check_equilateralism())
-            throw new RuntimeException("The triangle is not equilateral triangle !");
+            throw new RuntimeException(" element is not an equilateral triangle");
     }
 
     private boolean check_equilateralism() {
